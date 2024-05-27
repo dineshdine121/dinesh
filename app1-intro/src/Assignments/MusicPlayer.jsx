49279b0027cabd3352039f0c077a760a40e465f4
dineshdine121 <dineshdine.a121@gmail.com>
2024-05-27 20:03:51 +0530

    add

---
+++ app1-intro/src/Assignments/MusicPlayer.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class MusicPlayer extends Component{
+    state={
+        currentsong:anaganaga,
+        playlist:1
+    }
+
+
+    addplay=()=>{}
+    addpause=()=>{}
+    addskip=()=>{}
+}
