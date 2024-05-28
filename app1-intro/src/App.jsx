1770da7e48fa01da7169a6884157f6ce7738794f
dineshdine121 <dineshdine.a121@gmail.com>
2024-05-28 12:38:35 +0530

    add

--- app1-intro/src/App.jsx
+++ app1-intro/src/App.jsx
@@ -7,12 +7,15 @@ function App() {
   return <div>
     {/* <h1>MusicPlayer Component</h1>
     <MusicPlayer /> */}
-    <h1> RecipeBook components</h1>
-    <RecipeBook/>
+    {/* <h1> RecipeBook components</h1>
+    <RecipeBook/> */}
     {/* <h3>MovieDatabase Components</h3> */}
     {/* <MovieDatabase/> */}
-    {/* <h2>ChatApplication Component</h2>
-   <ChatApplication/> */}
+    <h2>ChatApplication Component</h2>
+   <ChatApplication/>
+   <h1> ims components</h1>
+   <Management/>
+
   </div>
 }
 export default App
---
+++ app1-intro/src/Assignments/Management.jsx
@@ -0,0 +1,47 @@
+import { Component } from "react";
+
+export class RecipeBook extends Component {
+    state = {
+        quantityavailable: "1",
+        pesron: {
+          productname:"cycle",
+        },
+       price: ["300","400"]
+    };
+
+
+    addnewproducts = () => {
+        const newstate = { ...this.state };
+        newstate.price.push("bike");
+        this.setState(newstate);
+    };
+    updatequantities= () => {
+        const newstate = { ...this.state };
+        newstate.price.splice(1, 0, "20");
+        this.setState(newstate);
+    };
+    removeproducts = () => {
+        const newstate = { ...this.state };
+        newstate.price.pop();
+        this.setState(newstate);
+    };
+
+    render() {
+        return (
+            <div>
+
+                <button onClick={this.addnewproducts}>addnewproducts</button>
+                <button onClick={this.updatequantities}>updatequantities</button>
+                <button onClick={this.removeproducts}>removeproducts</button>
+
+                <ul>
+                    {this.state.price.map((val) => <li>{val}</li>)}
+                </ul>
+                <ul>
+                    <p>{this.state.pesron.productname}</p>
+                    <p>{this.state.pesron.price}</p>
+                </ul>
+            </div>
+        )
+    }
+}
