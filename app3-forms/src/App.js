38d0e51a360066df1f03479f31875a20d95627cd
dineshdine121 <dineshdine.a121@gmail.com>
2024-06-13 11:13:03 +0530

    add

---
+++ app3-forms/src/App.js
@@ -0,0 +1,7 @@
+import { User } from "./Assignments/User";
+import { User1 } from "./Assignments/User1";
+
+{/* <User/> */}
+
+
+<User1/>
--- app3-forms/src/index.js
+++ app3-forms/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { User } from './Assignments/User';
+import { User1 } from './Assignments/User1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <User/> */}
+    <User1/>
+
   </React.StrictMode>
 );
 
