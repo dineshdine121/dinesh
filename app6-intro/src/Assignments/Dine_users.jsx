2bc4cab688a511615898536bccdc00fb8c334fb4
dineshdine121 <dineshdine.a121@gmail.com>
2024-06-26 08:07:19 +0530

    added

---
+++ app6-intro/src/Assignments/Dine_users.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from 'axios'
+import Dineform from "./Dine_form";
+import Dinetable from "./Dine_table";
+const Dine = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [allUsers, setAllUsers] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+
+  useEffect(() => {
+    getUsersFromServer()
+  }, [])
+  const createUser = () => {
+    axios.post("http://localhost:4200/dine_users", user).then(() => {
+      console.log("User Added Successfully !!!");
+      clearUser();
+      getUsersFromServer();
+    });
+  };
+  const editUser = (usr) => {
+    setUser(usr);
+    setIsEdit(true);
+  };
+  const deleteUser = (usr) => {
+    axios.delete("http://localhost:4200/dine_users" + usr.id).then(() => {
+      getUsersFromServer();
+    })
+  };
+  const updateUser = () => {
+    axios.put("http://localhost:4200/dine_users" + user.id, user).then(() => {
+      getUsersFromServer();
+      clearUser();
+      setIsEdit(false);
+    })
+  };
+  const clearUser = () => {
+    setUser({
+      name: "",
+      email: "",
+      password: "",
+    });
+  };
+  const getUsersFromServer = () => {
+    axios.get("http://localhost:4200/dine_users").then(({ data }) => {
+      setAllUsers(data)
+    })
+  };
+  const handleChange = (e) => {
+    const newUser = { ...user };
+    newUser[e.target.name] = e.target.value;
+    setUser(newUser);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <Dineform
+            handleChange={handleChange}
+            user={user}
+            createUser={createUser}
+            isEdit={isEdit}
+            updateUser={updateUser}
+          />
+        </div>
+        <div className="col-8">
+          <Dinetable
+            allUsers={allUsers}
+            editUser={editUser}
+            deleteUser={deleteUser} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dine;
