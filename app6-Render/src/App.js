bd29b2d3071bea3a397cfbb752b14dcfc77f1e1f
dineshdine121 <dineshdine.a121@gmail.com>
2024-07-03 14:32:05 +0530

    add

--- app6-Render/src/App.js
+++ app6-Render/src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import ClickCounter from './Render Method/ClickCounter';
-import HoverCounter from './Render Method/HoverCounter';
 import Main from './Render Method/Main';
 
 function App() {
