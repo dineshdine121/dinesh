4c4b4679fcc8c0ba80cd706f90f2247c20fa620a
dineshdine121 <dineshdine.a121@gmail.com>
2024-07-07 23:00:23 +0530

    add

--- app9-React-SPA/src/App.js
+++ app9-React-SPA/src/App.js
@@ -5,7 +5,7 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Navbar from './Components/Navbar';
-
+import PageNotFound from "./Components/PageNotFound";
 function App() {
   return (
     <div className="App">
@@ -15,7 +15,9 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
        </Routes>
+
       </Router>
     {/* <Home/>
     <About/>
