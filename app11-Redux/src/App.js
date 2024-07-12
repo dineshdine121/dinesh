0e2b67a0a73cf28b4768ecfb5034f8a5fcdec81e
dineshdine121 <dineshdine.a121@gmail.com>
2024-07-12 10:28:52 +0530

    add

--- app11-Redux/src/App.js
+++ app11-Redux/src/App.js
@@ -1,8 +1,14 @@
 import react from 'react'
+import { useSelector } from 'react-redux'
 const App =()=>{
+  const users = useSelector((state)=>state.usersDetails.users)
+ console.log(users)
   return(
     <div>
       <h2> Welcome To Redux Toolkit</h2>
+   <ul>
+    {users.map((usr)=> <li></li>)}
+   </ul>
     </div>
   )
 }
--- app11-Redux/src/Store/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { UsersSlice } from './UsersSlice'
-
-export const Store = configureStore({
-    reducer:{
-    users:UsersSlice
-    }
-})
