6182b989e78561add53cc77086f42e140c9bfa9e
dineshdine121 <dineshdine.a121@gmail.com>
2024-08-22 12:44:01 +0530

    add

---
+++ javascript_30projects/project8-To-Do-ListApp/script.js
@@ -0,0 +1,26 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+function addTask() {
+    if(inputBox.value === ''){
+        alert("You Must Write Something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+
+    }
+    inputBox.value = ""
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagname === "LI"){
+        e.target.classlist.toggle("checked");
+    }
+    else if(e.target.tagname === "SPAN"){
+        e.target.parentelement.remove();
+    }
+},false);
