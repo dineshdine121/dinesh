7e20f0a0de4b12fe04a27d88d28045181e04c916
dineshdine121 <dineshdine.a121@gmail.com>
2024-09-01 10:46:46 +0530

    add

---
+++ javascript_30projects/project13-DarkMode/script.js
@@ -0,0 +1,6 @@
+
+var darkt = document.getElementById("dark")
+var light = document.getElementById("LIGHT")
+darkt.onclick = function () {
+    document.body.classList.toggle("dark-teme");
+}
---
+++ javascript_30projects/project19-ToastNotification/script.js
@@ -0,0 +1,21 @@
+let toastBox = document.getElementById('toast-box')
+let succesMsg = ' <i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> please fix the error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+function showToast(msg) {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if (msg.includes('error')) {
+        toast.classList.add('error');
+    }
+    if (msg.includes('Invalid')) {
+        toast.classList.add('Invalid');
+    }
+    setTimeout(() => {
+        toast.remove();
+
+    }, 6000);
+}
